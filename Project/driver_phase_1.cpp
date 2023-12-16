d1a80f84841704fd782ba128e5b216ef3ed54f86
KelvinAntigua <92489715+KelvinAntigua@users.noreply.github.com>
2023-12-16 20:41:40 +0000

    git commit

--- Project/driver_phase_1.cpp
+++ Project/driver_phase_1.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>  // For std::find
+#include "Food.h"
+#include "Customer.h"
+
 
 int main() 
 {   
     std::cout << std::endl;
 
-    // instantiate A container
-    // instantiate B container
-    // instantiate A container iterator
-    // instantiate B container iterator
+     // Instantiate food container
+    std::vector<Food> foodContainer;
+
+    // Instantiate customer container
+    std::vector<Customer> customerContainer;
+
+    // Instantiate food container iterator
+    std::vector<Food>::iterator foodIterator;
+
+    // Instantiate customer container iterator
+    std::vector<Customer>::iterator customerIterator;
+
+    // instantiate food container
+    // instantiate customer container
+    // instantiate food container iterator
+    // instantiate customer container iterator
     // read into A container
     // read into B container
 
